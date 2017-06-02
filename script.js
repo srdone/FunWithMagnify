afe227f02420e7930b2bf045e807223f65f73fe8
Stephen Done <sdone@simplifile.com>
2017-06-02 14:28:01 -0600

    add Rx, let pointer events fall through overlaid images

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ function init() {
     var image = document.getElementsByClassName("display-image");
     console.log(image);
 
+    var mouseMoves = Rx.Observable.fromEvent(image[0], 'mousemove');
+
+    mouseMoves.subscribe(console.log);
+
     magnify(image[0], {debug: true, speed: 1000, src: "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg"});
 }
