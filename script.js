7c277f87dcc64f86079ba6dba203cea08c2f3f4f
Stephen Done <sdone@simplifile.com>
2017-06-05 16:27:17 -0600

    correct math pseudocode. Note that current status of magnify.js is broken

--- script.js
+++ script.js
@@ -3,26 +3,30 @@ window.onload = init;
 function init() {
     var image = document.getElementsByClassName("display-image")[0];
     var stampImage = document.getElementsByClassName("stamp-image")[0];
+    var imageContainer = document.getElementsByClassName('image-container')[0];
 
-    var mouseMoves = Rx.Observable.fromEvent(image, 'mousemove');
-    var mouseEnter = Rx.Observable.fromEvent(image, 'mouseenter');
-    var mouseLeave = Rx.Observable.fromEvent(image, 'mouseleave');
-
-//    mouseMoves.subscribe(console.log);
-
-    magnify([image, stampImage], {debug: true, speed: 1000, src: [
-        "https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg",
-        "https://ak6.picdn.net/shutterstock/videos/6696536/thumb/5.jpg"
-    ]});
+    var mouseMoves$ = Rx.Observable.fromEvent(image, 'mousemove');
+    var mouseEnter$ = Rx.Observable.fromEvent(image, 'mouseenter');
+    var mouseLeave$ = Rx.Observable.fromEvent(image, 'mouseleave');
+    
 }
 
 //Pseudocode explaining how the positioning works
 /*
     mouseDistanceFromLeft: mouseX - boundingBoxLeft
     mouseDistanceFromTop: mouseY - boundingBoxTop
+    imageRatio: magnifiedMainImageWidth / mainImageWidth
 
-    lens left: mouseDistanceFromLeft - halfLensOffsetWidth;
-    lens top: mouseDistanceFromTop - halfLensOffsetHeight;
-    backgroundX: -1 * (mouseDistanceFromLeft / (imageWidth * magnifiedImageWidth) - halfLensOffsetWidth);
-    backgroundY: -1 * (mouseDistanceFromTop / (imageHeight * magnifiedImageHeight) - halfLensOffsetHeight);
+
+    lens left: mouseDistanceFromLeft - halfLensWidth;
+    lens top: mouseDistanceFromTop - halfLensHeight;
+
+    //Positioning for mainImage
+    backgroundX: -1 * (mouseDistanceFromLeft * imageRatio - halfLensWidth);
+    backgroundY: -1 * (mouseDistanceFromTop * imageRatio - halfLensHeight);
+
+    //Positioning for stampImage
+    backgroundX: -1 * (stampDistanceFromMainImageLeft * imageRatio + mouseDistanceFromLeft * imageRatio - halfLensWidth)
+    backgroundY: -1 * (stampDistanceFromMainImageTop * imageRatio + mouseDistanceFromTop * imageRatio - halfLensHeight)
 */
+
